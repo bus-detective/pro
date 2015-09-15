53432e529c77ac32fee6fa78e219485456996096
Kevin Rockwood <kevin@rockwood.me>
2015-09-15 10:42:28 -0400

    Only collect in production

--- server/config/config.exs
+++ server/config/config.exs
@@ -19,10 +19,6 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-config :quantum, cron: [
-  "* * * * *": { BdPro.Collector, :collect }
-]
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
