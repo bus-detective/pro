dbae42a0c575d40c81b1090acc3103ec3650275f
Kevin Rockwood <kevin@rockwood.me>
2015-09-17 08:53:09 -0400

    Rework report around campaignReportFilters service
    This service will maintain state of what filters the user selects.

--- client/app/models/campaign-report.js
+++ client/app/models/campaign-report.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
-import moment from 'moment';
-let { computed, ObjectProxy } = Ember;
 
 export default Ember.Object.extend({
-  vehicleFilters: computed.map('vehicles', (vehicle) => {
-    return ObjectProxy.create({ content: vehicle, isSelected: true });
-  }),
-  selectedVehicles:  computed.filterBy('vehicleFilters', 'isSelected', true),
-  selectedVehicleIds:  computed.mapBy('selectedVehicles', 'id')
 });
