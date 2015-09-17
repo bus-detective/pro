8ac475a8fb0340fcd3d4f239b58cab8f6f628f13
Kevin Rockwood <kevin@rockwood.me>
2015-09-17 17:20:56 -0400

    Pass only selected vehiclePositions into map
    This makes things a lot easier to reason about because the map 
    re-renders each time data changes. It's a little slower, but I think 
    it's worth it.

--- client/app/models/campaign-report.js
+++ client/app/models/campaign-report.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
+let { computed } = Ember;
 
 export default Ember.Object.extend({
+  vehicles: [],
+  vehiclePositions: [],
+  selectedVehicles: computed.filterBy('vehicles', 'isSelected', true),
+
+  selectedVehiclePositions: computed('selectedVehicles', function() {
+    let selectedVehicleRemoteIds = this.get('selectedVehicles').mapBy('remoteId');
+    return this.get('vehiclePositions').filter((vehiclePosition) => {
+      return selectedVehicleRemoteIds.contains(vehiclePosition.get('vehicleRemoteId'));
+    });
+  })
 });
