a8a23c476341f2cbeea3d0a6bc27ec23ef53f3d2
Kevin Rockwood <kevin@rockwood.me>
2015-09-23 15:56:05 -0400

    Use ::text to avoid date coercion

--- server/web/queries/demographic_query.ex
+++ server/web/queries/demographic_query.ex
@@ -8,17 +8,12 @@ defmodule BdPro.DemographicQuery do
       INNER JOIN vehicles ON vehicles.remote_id = vehicle_positions.vehicle_remote_id
       INNER JOIN campaigns ON campaigns.id = vehicles.campaign_id
         WHERE campaigns.id = $1
-        AND vehicle_positions.timestamp BETWEEN DATE($2) AND DATE($3)
+        AND vehicle_positions.timestamp BETWEEN DATE($2::text) AND DATE($3::text)
       GROUP BY tracts.id
   """
 
   def search(%{"campaign_id" => campaign_id, "start_date" => start_date, "end_date" => end_date}) do
-    Repo.execute(@sql, [String.to_integer(campaign_id), parse_date(start_date), parse_date(end_date)])
-  end
-
-  defp parse_date(date_string) do
-    {:ok, date} = DateFormat.parse(date_string, "{YYYY}-{0M}-{0D}")
-    {{date.year, date.month, date.day}, {0, 0, 0, 0}}
+    Repo.execute(@sql, [String.to_integer(campaign_id), start_date, end_date])
   end
 end
 
