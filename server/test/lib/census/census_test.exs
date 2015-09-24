33d0ffb4885c3dcd638cb57933bcfb09325483a4
Kevin Rockwood <kevin@rockwood.me>
2015-09-24 13:48:04 -0400

    Add census tract fetching

---
+++ server/test/lib/census/census_test.exs
@@ -0,0 +1,8 @@
+defmodule BdPro.CensusTest do
+  use ExUnit.Case, async: true
+
+  test "fetch_tract: fetches information for the tract" do
+    result = BdPro.Census.fetch_tracts(state_id: 39, get: ["Tot_Population_ACS_09_13"])
+    assert hd(result)["Tot_Population_ACS_09_13"] == "5087"
+  end
+end
