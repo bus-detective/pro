38b639582cb6c464bf944b48ce01dbd0c40cae28
Alexanderia Heflin <alex@gaslight.co>
2015-10-05 15:08:10 -0400

    Refactor error handling to receive a property

--- client/app/pods/components/bd-inline-error/component.js
+++ client/app/pods/components/bd-inline-error/component.js
@@ -5,9 +5,6 @@ export default Ember.Component.extend({
   model: null,
   classNames: ['form-group'],
   classNameBindings: ['hasError'],
-  property: null,
-  hasError: Ember.computed.notEmpty('model.errors'),
-  defineObjectWithErrors: function() {
-    Ember.defineProperty(this, 'objectWithErrors', Ember.computed.alias('model.errors.' + this.get('property')));
-  }.on('init', 'objectWithErrors')
+  hasError: Ember.computed.notEmpty('errors'),
+  errors: []
 });
