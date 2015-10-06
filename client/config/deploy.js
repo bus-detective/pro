61e00956e19db104666be89180dbbb9924c959fa
Kevin Rockwood <kevin@rockwood.me>
2015-10-06 14:08:55 -0400

    Upgrade to new ember deploy
    This lets us auto-activate, YAY!

--- client/config/deploy.js
+++ client/config/deploy.js
@@ -1,19 +1,40 @@
-module.exports = {
-  production: {
-    store: {
-      type: "S3",
-      accessKeyId: process.env['S3_ID'],
+/* jshint node: true */
+
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {},
+
+    s3: {
+      accessKeyId: process.env['S3_KEY'],
       secretAccessKey: process.env['S3_SECRET'],
-      bucket: "busdetective-pro",
-      acl: 'public-read',
-      indexMode: "indirect",
+      bucket: process.env['S3_BUCKET'],
+      filePattern: "**/*.{js,css,png,gif,jpg,map,xml,txt,svg,eot,ttf,woff,woff2,html}"
     },
 
-    assets: {
-      type: "s3",
-      accessKeyId: process.env['S3_ID'],
-      secretAccessKey: process.env['S3_SECRET'],
-      bucket: 'busdetective-pro'
-    }
+    redis: {
+      host: process.env['REDIS_HOST'],
+      port: process.env['REDIS_PORT'],
+      password: process.env['REDIS_PASSWORD']
+    },
+  };
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    // configure other plugins for development deploy target here
   }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    // configure other plugins for staging deploy target here
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    // configure other plugins for production deploy target here
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
 };
