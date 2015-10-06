020fafeefbb3e2bf5c1004263508978b18ec57dd
Kenny Glenn <kenny@gaslight.co>
2015-10-06 10:20:02 -0400

    Styles for managing campaigns

--- client/app/pods/campaigns/index/controller.js
+++ client/app/pods/campaigns/index/controller.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     newCampaign() {
-      var campaign = this.get('store').createRecord('campaign', {name: "Untitled Campaign"});
+      var campaign = this.get('store').createRecord('campaign', {name: "New Campaign"});
       campaign.save().then((campaign) => {
         this.transitionToRoute('campaigns.edit', campaign);
       });
