a966f75bceaa96b36e1a25e1c18b2a665318c97e
Kenny Glenn <kenny@gaslight.co>
2015-10-08 14:12:36 -0400

    Styles for graph legend

--- client/app/pods/components/bd-legend/component.js
+++ client/app/pods/components/bd-legend/component.js
@@ -3,6 +3,7 @@ let { computed } = Ember;
 
 export default Ember.Component.extend({
   tagName: "li",
+  classNames: ["legend"],
   style: computed('background', function() {
     return new Ember.Handlebars.SafeString("background-color: " + this.get('background'));
   })
