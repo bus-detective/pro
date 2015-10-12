db2908311323bc4c15f9f7996a18bcead4f46852
Kevin Rockwood <kevin@rockwood.me>
2015-10-12 10:15:19 -0400

    Refactor managing vehicles feature test

--- test/features/managing_vehicles_test.exs
+++ test/features/managing_vehicles_test.exs
@@ -1,14 +1,19 @@
 defmodule BdPro.ManagingVehiclesFeature do
   use BdPro.FeatureCase
+  import BdPro.Factories
   alias BdPro.CampaignIndexPage
   alias BdPro.CampaignEditPage
 
   setup do
-    {:ok, %{campaign: BdPro.CampaignFactory.create}}
+    {:ok, user: sign_in_generic_user}
+  end
+
+  setup do
+    {:ok, campaign: create(:campaign) }
   end
 
   setup %{campaign: campaign} do
-    {:ok, %{vehicle: BdPro.VehicleFactory.create(campaign_id: campaign.id )}}
+    {:ok, vehicle: create(:vehicle, campaign: campaign)}
   end
 
   test "Viewing vehicle count on a campaign", %{campaign: campaign} do
@@ -18,20 +23,15 @@ defmodule BdPro.ManagingVehiclesFeature do
   end
 
   test "Adding vehicles to a campaign", %{campaign: campaign} do
-    new_vehicle_attrs = %{remote_id: "1234", product: "Kong Wrap"}
     CampaignEditPage.visit(campaign)
-    vehicle_count = CampaignEditPage.vehicle_count
-    CampaignEditPage.add_vehicle_and_save(new_vehicle_attrs)
-    updated_vehicle_count = vehicle_count + 1
-    assert CampaignEditPage.vehicle_count == updated_vehicle_count
+    CampaignEditPage.add_vehicle_and_save(%{remote_id: "1234", product: "Kong Wrap"})
+    assert CampaignEditPage.vehicle_count == 3
   end
 
   test "Delete vehicles from a campaign", %{campaign: campaign, vehicle: vehicle} do
     CampaignEditPage.visit(campaign)
-    vehicle_count = CampaignEditPage.vehicle_count
     CampaignEditPage.delete_vehicle(vehicle)
-    updated_vehicle_count = vehicle_count - 1
-    assert CampaignEditPage.vehicle_count == updated_vehicle_count
+    assert CampaignEditPage.vehicle_count == 1
   end
 
   test "Editing a vehicle", %{campaign: campaign, vehicle: vehicle} do
@@ -42,9 +42,8 @@ defmodule BdPro.ManagingVehiclesFeature do
   end
 
   test "Error messages display", %{campaign: campaign, vehicle: vehicle} do
-    new_vehicle_attrs = %{remote_id: "", product: "King Kong Wrap"}
     CampaignEditPage.visit(campaign)
-    CampaignEditPage.edit_vehicle_and_save(vehicle, new_vehicle_attrs)
+    CampaignEditPage.edit_vehicle_and_save(vehicle, %{remote_id: "", product: "King Kong Wrap"})
     assert CampaignEditPage.has_error?
   end
 end
