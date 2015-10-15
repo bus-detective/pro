586c3a514ea7bc27efdcc09e7d1c1fe0d2b0cc88
Kevin Rockwood <kevin@rockwood.me>
2015-10-15 17:19:48 -0400

    Lint

--- client/app/helpers/bd-currency.js
+++ client/app/helpers/bd-currency.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 function format(number) {
   return parseFloat(number).toFixed(0).replace(/./g, function(c, i, a) {
     return i && c !== "." && ((a.length - i) % 3 === 0) ? ',' + c : c;
-  })
+  });
 }
 
 export function bdCurrency(number) {
