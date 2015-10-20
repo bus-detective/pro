3cf3c305e097667ec657a7ba664f595ea9e258ed
Alexanderia Heflin <alex@gaslight.co>
2015-10-20 11:18:19 -0400

    Format lat and lng for shape points

--- web/views/zip_code_view.ex
+++ web/views/zip_code_view.ex
@@ -10,8 +10,20 @@ defmodule BdPro.ZipCodeView do
   def render("zip_code.json", %{zip_code: [zip_code, count]}) do
     %{
       id: zip_code.id,
-      zip_code: zip_code.zip_code,
-      count: count
+      code: zip_code.zip_code,
+      count: count,
+      shape: format_shape(zip_code.shape)
     }
+  end
+
+  defp format_shape(shape) do
+    shape = Geo.JSON.encode(shape)
+    coords = shape["coordinates"]
+    new_shape = Enum.map(coords, fn(sub_coordinates) ->
+      Enum.map(sub_coordinates, fn(third_sub_coordinates) ->
+        Enum.map(third_sub_coordinates, &Enum.reverse(&1))
+      end)
+    end)
+    %Geo.MultiPolygon{ coordinates: new_shape }
   end
 end
