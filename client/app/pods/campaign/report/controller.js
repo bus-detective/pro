5ebec4a2511ef625ac0c2c7dd5e0c14ec3d75842
Alexanderia Heflin <alex@gaslight.co>
2015-10-21 15:30:04 -0400

    Fix report query params automatically resetting themselves

--- client/app/pods/campaign/report/controller.js
+++ client/app/pods/campaign/report/controller.js
@@ -1,11 +1,10 @@
 import Ember from 'ember';
-import moment from 'moment';
 let { observer } = Ember;
 
 export default Ember.Controller.extend({
   queryParams: ['startDate', 'endDate'],
-  startDate: moment().subtract(2, 'weeks').format("YYYY-MM-DD"),
-  endDate: moment().format("YYYY-MM-DD"),
+  startDate: null,
+  endDate: null,
 
   startDateDidChange: observer('model.startDate', function(){
     this.set('startDate', this.get('model.startDate'));
@@ -15,5 +14,3 @@ export default Ember.Controller.extend({
     this.set('endDate', this.get('model.endDate'));
   })
 });
-
-
